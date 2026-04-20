5e7da2559f7239f3d8b766ad7fc1201d8221dab5
sandi2024 <143309738+sandi2024@users.noreply.github.com>
2026-04-20 20:10:42 +0000

    configuracion de pagina

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+from datetime import datetime 
 def main():
     
-    st.title("🎈 Kardex")
+    st.set_page_config(
+        page_title="ANALSIS DE DATOS: KARDEX",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+   
+    st.title("Kardex")
     st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
     )
