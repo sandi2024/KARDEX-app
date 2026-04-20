fe695d2e03812c2fee134699ae506b687da87de3
sandi2024 <143309738+sandi2024@users.noreply.github.com>
2026-04-20 19:56:15 +0000

    cambio main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 Kardex")
-st.write(
+def main():
+    
+    st.title("🎈 Kardex")
+    st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    )
+
+
+if __name__ == '__main__':
+    main()
