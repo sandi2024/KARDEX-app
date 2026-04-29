0eee1f2a7923c5b63dd21e881991e93cdd18b665
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-04-29 16:02:21 -0700

    importe ny

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
+
 import json
 
 # Configuración de página
