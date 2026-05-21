2967eb4743cde2185d4ec70be17800e400c82212
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-21 10:00:00 -0700

    Update riesgo_academico.py

--- pages/riesgo_academico.py
+++ pages/riesgo_academico.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from src.database import fetch_analisis_reprobacion # La consulta SQL que hicimos antes
-from src.analysis import calcular_indice_riesgo
+from src.analisis import calcular_indice_riesgo
 from src.utils import load_css, render_header, create_uabc_metric_card
 
 load_css()
