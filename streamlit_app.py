f6c36a22fa94a81216d4c3510e4fe6ed1d6c3a8e
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-25 13:04:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from src.utils import load_css, render_header, render_footer, create_uabc_metric_card
-from src.queries import fetch_analisis_reprobacion, fetch_kardex_alumno 
+from src.queries import fetch_analisis_reprobacion, get_kardex_alumno 
 from src.analisis import calcular_indice_riesgo
 
 import numpy as np
@@ -16,7 +16,7 @@ render_header()
 # CARGA DE DATOS
 # ============================================
 
-df = calcular_indice_riesgo(fetch_kardex_alumno("001/32146"))
+df = calcular_indice_riesgo(get_kardex_alumno("001/32146"))
 
 # --- SIDEBAR COMPARTIDO ---
 with st.sidebar:
