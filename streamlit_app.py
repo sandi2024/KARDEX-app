9b63fae97b23910459df669ff3745fa3db8f257d
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-25 14:57:31 -0700

    archivo toml

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,12 @@ df = calcular_indice_riesgo(get_kardex_alumno("001/32146"))
 with st.sidebar:
     st.image("assets/UABC-logo.png", width=150)
     st.markdown("### Panel de Control")
+    with st.sidebar:
+    st.image("logo.png", width=200) # APARECE PRIMERO
+    st.page_link("pages/carreras.py", label="Carreras", icon="📊") # APARECE DESPUÉS
+    st.page_link("pages/perfil_alumnos.py", label="Perfil de Alumnos", icon="🎓") # APARECE DESPUÉS
+    st.page_link("pages/riesgo_academico.py", label="Riesgo Académico", icon="🚨") # APARECE DESPUÉS
+    
 #    carrera_global = st.selectbox("📚 Carrera", ["Todas", "Ingeniería Química", "..."])
      # Periodo académico
     periodo = st.selectbox(
