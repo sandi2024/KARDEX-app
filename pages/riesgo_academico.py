59e7e92a9fb0f99e73f346812faffe992220971e
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-25 15:18:44 -0700

    general pagina

--- pages/riesgo_academico.py
+++ pages/riesgo_academico.py
@@ -12,7 +12,7 @@ render_header()
 with st.sidebar:
     st.image("assets/UABC-logo.png", width=150)
     st.markdown("### Panel de Control")
-    st.page_link("pages/streamlit_app.py", label="General") # APARECE DESPUÉS
+    st.sidebar.page_link("streamlit_app.py", label="Inicio", icon="🏠")
     st.page_link("pages/carreras.py", label="Carreras", icon="📊") # APARECE DESPUÉS
     st.page_link("pages/perfil_alumnos.py", label="Perfil de Alumnos", icon="🎓") # APARECE DESPUÉS
     st.page_link("pages/riesgo_academico.py", label="Riesgo Académico", icon="🚨") # APARECE DESPUÉS
