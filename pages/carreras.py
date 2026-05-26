dffc536522031a0a9710e8893402cba46a40c38e
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-26 13:21:55 -0700

    Update carreras.py

--- pages/carreras.py
+++ pages/carreras.py
@@ -12,7 +12,7 @@ if 'df_raw' in st.session_state:
     df_datos = st.session_state.df_raw
     st.write("Datos recuperados de la sesión con éxito.")
     # Aquí ya puedes usar df para tus gráficas de carrera
-elif 'df_raw' not in st.session_state:
+elif st.session_state.df_raw.empty:
     st.session_state.df_raw = get_data_analisis_completo()
     df_datos = st.session_state.df_raw
   #  st.warning("Por favor, inicia la aplicación en la página principal para cargar los datos.")
