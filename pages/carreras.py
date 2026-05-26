a498aba80dd44ceb1736c4eb9e1c245c156b842b
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-26 01:23:31 -0700

    Update carreras.py

--- pages/carreras.py
+++ pages/carreras.py
@@ -63,7 +63,7 @@ if carrera_sel == "Todas las carreras":
     df_filtrado_carrera = df_filtrado_periodo
 else:
     # Filtramos el DataFrame original en memoria (es instantáneo)
-    df_filtrado = df_filtrado_periodo[df_filtrado_periodo['carrera'] == carrera_sel]
+    df_filtrado_carrera = df_filtrado_periodo[df_filtrado_periodo['carrera'] == carrera_sel]
 
 df_final = procesar_academicos(df_filtrado_carrera, umbral)   #Segun perido y umbral seleccionado
 
