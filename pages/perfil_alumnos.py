bdf3f36bcd83dd0dc6d3e2b1f5b54d406909698c
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-26 17:56:34 -0700

    Update perfil_alumnos.py

--- pages/perfil_alumnos.py
+++ pages/perfil_alumnos.py
@@ -68,16 +68,16 @@ if matricula:
         id_carrera = num_carreras['id_carrera'].iloc[0]
 
     # 2. Obtener materias filtradas por matrícula Y carrera
-    df_periodos = fetch_detalle_por_periodo(matricula, id_carrera)
+ #   df_periodos = fetch_detalle_por_periodo(matricula, id_carrera)
 
     # 3. Código del Análisis por Periodo
-    st.subheader(f"Análisis de Rendimiento por Periodo")
+  #  st.subheader(f"Análisis de Rendimiento por Periodo")
     
     # Agrupamos para obtener el promedio por periodo
-    df_stats_periodo = df_periodos.groupby('id_periodo').agg({
-        'calificacion': 'mean',
-        'materia': 'count'
-    }).reset_index()
+  #  df_stats_periodo = df_periodos.groupby('id_periodo').agg({
+ #       'calificacion': 'mean',
+ #       'materia': 'count'
+ #   }).reset_index()
     
     ####################### INFORMACION GENERAL ############################################333
 
@@ -141,15 +141,15 @@ if matricula:
 
     ###############################  GRAFICA
     # Gráfica de evolución
-    fig = px.line(df_stats_periodo, x='id_periodo', y='calificacion', 
-                  title="Evolución del Promedio Académico",
-                  markers=True, text="calificacion")
-    fig.update_traces(textposition="top center", line_color="#00723F") # Verde UABC
-    st.plotly_chart(fig, use_container_width=True)
+#    fig = px.line(df_stats_periodo, x='id_periodo', y='calificacion', 
+ #                 title="Evolución del Promedio Académico",
+ #                 markers=True, text="calificacion")
+ #   fig.update_traces(textposition="top center", line_color="#00723F") # Verde UABC
+ #   st.plotly_chart(fig, use_container_width=True)
     
     # Mostrar materias cursadas en ese periodo específico
-    st.write("### Detalle Cronológico")
-    st.table(df_periodos[['id_periodo', 'materia', 'calificacion']])
+ #   st.write("### Detalle Cronológico")
+ #   st.table(df_periodos[['id_periodo', 'materia', 'calificacion']])
 
 
 
