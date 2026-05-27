a072c29bffbef61292e2359af83d450430dfb156
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-26 18:43:04 -0700

    Update perfil_alumnos.py

--- pages/perfil_alumnos.py
+++ pages/perfil_alumnos.py
@@ -66,15 +66,19 @@ if matricula:
     
     if num_carreras > 1:
         st.warning("⚠️ Este alumno tiene registros en múltiples carreras.")
-        seleccion = st.selectbox("Selecciona la carrera para el análisis:", 
-                                 df_alumno['carrera'].unique())
-        id_carrera = df_alumno[df_alumno['carrera'] == seleccion]['id_carrera'].iloc[0]
+  #      seleccion = st.selectbox("Selecciona la carrera para el análisis:", 
+   #                              df_alumno['carrera'].unique())
+   #     id_carrera = df_alumno[df_alumno['carrera'] == seleccion]['id_carrera'].iloc[0]
+        lista_carreras = ["Todas las carreras"] + sorted(df_alumno['carrera'].unique().tolist())
+        carrera_sel = st.selectbox("📚 Seleccione carrera", lista_carreras)
+        df_alumno_carrera = df_alumno[df_alumno['carrera'] == carrera_sel]
     else:
         id_carrera = num_carreras['id_carrera'].iloc[0]
+        df_alumno_carrera = df_alumno
 
     # 2. Obtener materias filtradas por matrícula Y carrera
     st.write("id_carrera", id_carrera)
-    df_alumno_carrera = df_alumno[df_alumno['carrera'] == id_carrera]
+ #   df_alumno_carrera = df_alumno[df_alumno['carrera'] == id_carrera]
 
     # 3. Código del Análisis por Periodo
   #  st.subheader(f"Análisis de Rendimiento por Periodo")
