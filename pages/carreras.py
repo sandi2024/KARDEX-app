bd893f57a684af11cbaa46e491dbacb0da0ae1c8
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-29 16:03:08 -0700

    Update carreras.py

--- pages/carreras.py
+++ pages/carreras.py
@@ -73,7 +73,7 @@ df_distribucion = distribucion_calificaciones(df_carrera)
 
 # ============================================== MÉTRICAS PRINCIPALES ============================================
 
-metricas = calcular_metricas_generales(df_filtrados)
+metricas = calcular_metricas_generales(df_final)
 #total_alumnos = len(df_final)
 #sobresalientes = len(df_final[df_final['promedio_general'] >= 90])
 #en_riesgo = len(df_final[df_final['estatus'] == 'RIESGO'])
