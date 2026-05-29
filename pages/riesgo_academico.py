58a686ae5f958b72ef808b0ae09098c910e49960
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-29 16:18:40 -0700

    Update riesgo_academico.py

--- pages/riesgo_academico.py
+++ pages/riesgo_academico.py
@@ -106,7 +106,7 @@ if ocultar_bajos:
 # 3. Imprimir el resultado
 st.dataframe(
     df_mostrar,
-    column_order=("id_estudiante", "nivel_riesgo", "alerta_score"),
+    column_order=("id_estudiante", "nivel_riesgo", "alerta_score", "tasa_etraordinario"),
     use_container_width=True
 )
 
