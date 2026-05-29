a06c6ba80c7b298cc61ab365e3b0fb1d39bb0752
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-05-28 17:54:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ with col_der:
 
     # 4. Bar Chart: Volumen por Plan de Estudios (Escala continua)
     plan_data = df_final['id_plan_estudio'].value_counts().reset_index()
-    fig4 = px.bar(plan_data, x='plan_estudio', y='count',
+    fig4 = px.bar(plan_data, x='id_plan_estudio', y='count',
                   color='count', color_continuous_scale='Blues',
                   title='📚 Volumen por Plan de Estudios')
     st.plotly_chart(fig4, use_container_width=True)
