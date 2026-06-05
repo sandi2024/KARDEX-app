1d654a401ee674ef880d9e2c04040bec3dad88ea
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-06-04 22:00:37 -0700

    Update queries.py

--- src/queries.py
+++ src/queries.py
@@ -38,7 +38,7 @@ def get_data_analisis_completo():
     SELECT 
         aa.matricula,
         pe.nombre AS carrera,
-        p.id_periodo AS periodo,    # Esto es para poder filtrar por periodo en el análisis de riesgo, anio_periodo
+        p.id_periodo AS periodo,    
         a.nombre AS asignatura,
         a.creditos AS creditos_materia,
         aa.calificacion,
