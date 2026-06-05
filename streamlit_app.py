29bb4f67099655570217ec1c05c19c83061b7338
sandi2024 <sandi.roblero@uabc.edu.mx>
2026-06-05 00:57:31 -0700

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,7 +205,6 @@ with columna2:
         'conteo_extraordinarios': 'mean'
     }).round(2)
     metrics_table.columns = ['📊 Promedio', '📈 Avance Promedio', '📝 Extraordinarios']
-    #st.dataframe(metrics_table, use_container_width=True)
     st.dataframe(metrics_table.style.background_gradient(subset=['📊 Promedio'], cmap='Blues'))
     
     
